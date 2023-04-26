f90d74c7e1cc8ecfb67de442e4fb70356467f422
Danya <danya.sh.07@gmail.com>
2023-04-26 15:55:11 +0300

    first commit

---
+++ j.s/J.S/main.js
@@ -0,0 +1,26 @@
+//function info(word) {
+//  console.log(word + "!");
+//}
+
+//function summa(a, b) {
+//  var res = a + b;
+//  info(res)
+// }
+
+//summa(5, 7);
+
+//function summa(arr) {
+//  var sum = 0;
+//  for (var i = 0; i < arr.length; i++)
+//     sum += arr[i];
+
+//  console.log(sum);
+//}
+
+//var array = [6, 8, 1];
+
+//summa(array);
+
+
+//var arr = [5, 7, 8];
+//console.log(arr.length);
